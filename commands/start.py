045c44373b80ca3df27f5d3faccbdfac303205de
Adeeb Haider <91616102+AdeebHaider137@users.noreply.github.com>
2024-12-25 00:13:46 +0530

    Update start.py

--- commands/start.py
+++ commands/start.py
@@ -5,6 +5,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a start message to the user."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}! Use /help to see available commands.",
+        rf"Hi {user.mention_html()} Welcome to the Persist Ventures Bot! Use /help to see available commands.",
         reply_markup=ForceReply(selective=True),
     )
